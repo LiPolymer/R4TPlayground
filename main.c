076d81dd1c3e84c189de07f41bca243bf3842843
LiPolymer <fish@lipoly.ink>
2026-04-10 10:04:17 +0800

    feat：keyboardHandler

--- main.c
+++ main.c
@@ -26,13 +26,15 @@ void setLed(char arrange) {
 /* Segment Displayer */
 char segBuffer[8] = {0};
 
-int segCleanI;
+/// @brief 准备段码缓存
 void cleanSegBuffer() {
+    int segCleanI;
     for (segCleanI = 0; segCleanI < 8; segCleanI++){
         segBuffer[segCleanI] = 0xFF;
     }
 }
 
+/// @brief 段码数字查找表
 data char segNumberLut[10] = {
     0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90
 };
@@ -59,7 +61,7 @@ void segDisplay(char pos, char content) {
 /// @brief 段码屏守护程序 放置在主逻辑
 void segDaemon() {
     static int curPos = 0;
-
+    //一次执行只维护一位
     P0 = 0xFF;
 	funcSel(P0F_DSeg);
 	funcSel(P0F_NONE);
@@ -74,27 +76,80 @@ void segDaemon() {
 	if(curPos >= 8) curPos = 0;
 }
 
+/* Keyboard Handler */
+
+int btnDat[4][4] = {0}; 
+
+void btnScanCol(char col) {
+    if (!P30) {
+        btnDat[col][0]++;
+    } else btnDat[col][0] = 0;
+    if (!P31) {
+        btnDat[col][1]++;
+    } else btnDat[col][1] = 0;
+    if (!P32) {
+        btnDat[col][2]++;
+    } else btnDat[col][2] = 0;
+    if (!P33) {
+        btnDat[col][3]++;
+    } else btnDat[col][3] = 0;
+}
+
+/// @brief 清理键盘输入针脚
+void cleanMatrixScanner(){
+    P30 = 1;
+	P31 = 1;
+	P32 = 1;
+	P33 = 1;
+}
+
+/// @brief 键盘守护程序 放置在主逻辑
+void btnDaemon() {
+    static char curCol = 0;
+    //依旧一次只扫一列
+    char dcols[4] = {1,1,1,1};
+    dcols[curCol] = 0;
+    P44 = dcols[0];
+    P42 = dcols[1];
+    P35 = dcols[2];
+    P34 = dcols[3];
+    btnScanCol(curCol);
+    curCol++;
+    if (curCol >= 4) curCol = 0;
+}
+
+/// @brief 设定按键状态
+/// @param x 
+/// @param y 
+/// @param stat 
+void setBtnStat(char x, char y, int stat){
+    btnDat[x][y] = stat;
+}
+
+/// @brief 获取按键状态
+/// @param x 
+/// @param y 
+/// @return x,y 处按键状态
+int getBtnStat(char x, char y) {
+    return btnDat[x][y];
+}
+
 void Update(void) {
-    static int curv = 0;
-    static int freqC = 0;
-    if (freqC < 10000) freqC++;
-    else {
-        freqC = 0;
-        if (curv >= 8) curv = 0;
-        segDisplay(0,segNumberLut[curv]);
-        curv++;
-    }
+    char nu = getBtnStat(2,3)/2000;
+    segDisplay(0,segNumberLut[nu<9 ? nu : 8]);
 }
 
 
 /* Timing Logic */
 void main() {
     cleanSegBuffer();
+    cleanMatrixScanner();
     setLed(0xFF);
     while (true) {
-        /* master logic */
-        segDaemon();
+        /* main cycle */
+        btnDaemon();
         Update();
+        segDaemon();
         //Delay10ms();
     }
 }
